6402bc142efca95eb23bdfa306b652d89cf7f449
Christian Rivera <christianrivera@Christians-MBP.lan>
2015-03-16 19:42:44 -0400

    Principio de la app, To do: hacer que pare los gestures. mejorar como se mueve los labels que no sea hide

--- MateFix/MateFix/ViewController.swift
+++ MateFix/MateFix/ViewController.swift
@@ -10,6 +10,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var swipeLbl: UILabel!
+    @IBOutlet weak var welcomeLbl: UILabel!
+    @IBOutlet weak var singInBtn: UIButton!
+    @IBOutlet weak var iphoneStart: UIImageView!
+    
+    @IBAction func singupGesture(sender: UIPanGestureRecognizer) {
+        iphoneStart.frame = CGRect(x: iphoneStart.frame.origin.x, y: iphoneStart.frame.origin.y / 1.050, width: iphoneStart.frame.size.width, height: iphoneStart.frame.size.height)
+        singInBtn.hidden = true
+        swipeLbl.hidden = true
+        welcomeLbl.hidden = true
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -17,7 +28,7 @@ class ViewController: UIViewController {
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        // Dispose of any resources that can be recreated .
     }
 
 
