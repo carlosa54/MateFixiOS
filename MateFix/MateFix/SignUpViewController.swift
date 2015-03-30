9320b1e227aaa64443e4fe0d3e76e5023a8e0e15
Carlos Ramirez <carlos.ramirez16@upr.edu>
2015-03-29 21:50:53 -0400

    Added InitialSignUpViewController

--- MateFix/MateFix/SignUpViewController.swift
+++ MateFix/MateFix/SignUpViewController.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import Alamofire
+
 
 class SignUpViewController: UIViewController {
 
@@ -15,6 +17,7 @@ class SignUpViewController: UIViewController {
     @IBOutlet weak var passwordTextField: DesignableTextField!
     @IBOutlet weak var emailTextField: DesignableTextField!
     @IBOutlet weak var signUpButton: DesignableButton!
+    var registerEndPoint = "https://matefix.herokuapp.com/users/register"
     
     override func touchesBegan(touches: NSSet, withEvent event: UIEvent) {
         self.view.endEditing(true)
@@ -22,7 +25,7 @@ class SignUpViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+       
         // Do any additional setup after loading the view.
     }
 
@@ -31,86 +34,121 @@ class SignUpViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-    func post(params: NSDictionary, url: String, postCompleted : (succeeded: Bool, msg: String) -> ()){
-        
-        PKNotification.loading(true)  // show loading view.
-        
-        var request = NSMutableURLRequest(URL: NSURL(string: url)!)
-        var session = NSURLSession.sharedSession()
-        request.HTTPMethod = "POST"
-        
-        
-        
-        var err: NSError?
-        request.HTTPBody = NSJSONSerialization.dataWithJSONObject(params, options: nil, error: &err)
-        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
-        request.addValue("application/json", forHTTPHeaderField: "Accept")
-        
-        var task = session.dataTaskWithRequest(request, completionHandler: {data, response, error -> Void in
-            println("Response: \(response)")
-            var strData = NSString(data: data, encoding: NSUTF8StringEncoding)
-            println("Body: \(strData)")
-            var err: NSError?
-            var json = NSJSONSerialization.JSONObjectWithData(data, options: .MutableLeaves, error: &err) as? NSDictionary
-            
-            var msg = "No Message"
-            
-            // Did the JSONObjectWithData constructor return an error? If so, log the error to the console
-            if(err != nil) {
-                println(err!.localizedDescription)
-                let jsonStr = NSString(data: data, encoding: NSUTF8StringEncoding)
-                postCompleted(succeeded: false, msg: "Error!")
-                println("Error could not parse JSON: '\(jsonStr)'")
-            }
-            else {
-                // The JSONObjectWithData constructor didn't return an error. But, we should still
-                // check and make sure that json has a value using optional binding.
-                if let parseJSON = json {
-                    // Okay, the parsedJSON is here, let's get the value for 'success' out of it
-                      PKNotification.loading(false) // hide loading view.
-                      postCompleted(succeeded: true, msg: "Logged in")
-                }
-                else {
-                    // Woa, okay the json object was nil, something went worng. Maybe the server isn't running?
-                    let jsonStr = NSString(data: data, encoding: NSUTF8StringEncoding)
-                    println("Error could not parse JSON: \(jsonStr)")
-                }
-            }
-        })
-        
-        task.resume()
-    }
+//    func post(params: NSDictionary, url: String, postCompleted : (succeeded: Bool, msg: String) -> ()){
+//        
+//        PKNotification.loading(true)  // show loading view.
+//        
+//        var request = NSMutableURLRequest(URL: NSURL(string: url)!)
+//        var session = NSURLSession.sharedSession()
+//        request.HTTPMethod = "POST"
+//        
+//        
+//        
+//        var err: NSError?
+//        request.HTTPBody = NSJSONSerialization.dataWithJSONObject(params, options: nil, error: &err)
+//        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+//        request.addValue("application/json", forHTTPHeaderField: "Accept")
+//        
+//        var task = session.dataTaskWithRequest(request, completionHandler: {data, response, error -> Void in
+//            println("Response: \(response)")
+//            var strData = NSString(data: data, encoding: NSUTF8StringEncoding)
+//            println("Body: \(strData)")
+//            var err: NSError?
+//            var json = NSJSONSerialization.JSONObjectWithData(data, options: .MutableLeaves, error: &err) as? NSDictionary
+//            
+//            var msg = "No Message"
+//            
+//            // Did the JSONObjectWithData constructor return an error? If so, log the error to the console
+//            if(err != nil) {
+//                println(err!.localizedDescription)
+//                let jsonStr = NSString(data: data, encoding: NSUTF8StringEncoding)
+//                postCompleted(succeeded: false, msg: "Error!")
+//                println("Error could not parse JSON: '\(jsonStr)'")
+//            }
+//            else {
+//                // The JSONObjectWithData constructor didn't return an error. But, we should still
+//                // check and make sure that json has a value using optional binding.
+//                if let parseJSON = json {
+//                    // Okay, the parsedJSON is here, let's get the value for 'success' out of it
+//                      PKNotification.loading(false) // hide loading view.
+//                      postCompleted(succeeded: true, msg: "Logged in")
+//                }
+//                else {
+//                    // Woa, okay the json object was nil, something went worng. Maybe the server isn't running?
+//                    let jsonStr = NSString(data: data, encoding: NSUTF8StringEncoding)
+//                    println("Error could not parse JSON: \(jsonStr)")
+//                }
+//            }
+//        })
+//        
+//        task.resume()
+//    }
     
 
     @IBAction func signUpButton(sender: DesignableButton) {
         
+        PKNotification.loading(true)  // show loading view.
         var email = emailTextField.text
         var password = passwordTextField.text
         var firstName = nameTextField.text
         var lastName = lastNameTextField.text
         
         
-       var params = ["email":email, "password":password, "first_name":firstName, "last_name": lastName, "about_me": "", "street_1": "Testing 101", "country": "PR", "city": "test", "zip_code": "00667"] as Dictionary<String,String>
+        var params = ["email":email, "password":password, "first_name":firstName, "last_name": lastName, "about_me": "", "street_1": "Testing 101", "country": "PR", "city": "test", "zip_code": "00667"] as Dictionary<String,String>
         
             signUpButton.enabled = false
         
-            post(params, url: "https://matefix.herokuapp.com/users/register") { (succeeded: Bool, msg: String) -> () in
-            var alert = UIAlertView(title: "Success!", message: msg, delegate: nil, cancelButtonTitle: "Okay.")
-            if(succeeded) {
-                alert.title = "Success!"
-                alert.message = msg
+        
+        Alamofire.request(.POST , registerEndPoint, parameters: params, encoding: .JSON )
+        .responseJSON { (request, response, data, error) in
+            if let anError = error
+            {
+                // got an error in getting the data, need to handle it
+                println("error calling POST")
+                println(error)
             }
-            else {
-                alert.title = "Failed : ("
-                alert.message = msg
+            else if let data: AnyObject = data
+            {
+                // handle the results as JSON, without a bunch of nested if loops
+                let post = JSON(data)
+                // to make sure it posted, print the results
+                println("The post is: " + post.description)
+                PKNotification.loading(false) // hide loading view.
             }
-            
-            // Move to the UI thread
-            dispatch_async(dispatch_get_main_queue(), { () -> Void in
-                // Show the alert
-                alert.show()
-            })
         }
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+//            post(params, url: "https://matefix.herokuapp.com/users/register") { (succeeded: Bool, msg: String) -> () in
+//            var alert = UIAlertView(title: "Success!", message: msg, delegate: nil, cancelButtonTitle: "Okay.")
+//            if(succeeded) {
+//                alert.title = "Success!"
+//                alert.message = msg
+//            }
+//            else {
+//                alert.title = "Failed : ("
+//                alert.message = msg
+//            }
+//        
+//            // Move to the UI thread
+//            dispatch_async(dispatch_get_main_queue(), { () -> Void in
+//                // Show the alert
+//                alert.show()
+//            })
+//        }
         
         
         
