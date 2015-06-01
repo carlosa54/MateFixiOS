5cc75c60838b1afb925572b79baa19641e8b7f8c
Christian Rivera <chris.riveradiaz@gmail.com>
2015-06-01 13:07:49 -0400

    ProjectsTableViewController UI, AutoLayout, And Clases Added.

---
+++ MateFix/MateFix/ProjectsTableViewCell.swift
@@ -0,0 +1,21 @@
+//
+//  ProjectsTableViewCell.swift
+//  MateFix
+//
+//  Created by Christian Rivera on 6/1/15.
+//  Copyright (c) 2015 Carlos Ramirez. All rights reserved.
+//
+
+import UIKit
+
+class ProjectsTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var titleProjectLabel: UILabel!
+    @IBOutlet weak var messageProjectLabel: UILabel!
+    @IBOutlet weak var locationProjectLabel: UILabel!
+    @IBOutlet weak var costProjectLabel: UILabel!
+    @IBOutlet weak var profilePicImage: UIImageView!
+
+    
+
+}
